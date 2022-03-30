c4da981a7dec82a833859485950e6d3a8067d759
kineticcat <84404038+kineticneticat@users.noreply.github.com>
2022-03-30 08:15:40 +0000

    added win detection

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-#the name of this game is noughts and crosses
 from tkinter import *
 from tkinter import ttk
-import sys, os
 
 root = Tk()
 
@@ -30,7 +28,6 @@ class _button:
     def update_text(self, text):
         squares[self.x][self.y] = text
         self.text.set(text)
-        print(squares)
     def test(self):
         return [self.x, self.y, self.text, self.loc]
     def press(self):
@@ -43,13 +40,22 @@ class _button:
         elif turn == 'o':
             self.update_text('o')
             turn = 'x'
+        if (squares[0][0] == 'x' and squares[0][1] == 'x' and squares[0][2] == 'x') or (squares[1][0] == 'x' and squares[1][1] == 'x' and squares[1][2] == 'x') or (squares[2][0] == 'x' and squares[2][1] == 'x' and squares[2][2] == 'x') or (squares[0][0] == 'x' and squares[1][0] == 'x' and squares[2][0] == 'x') or (squares[0][1] == 'x' and squares[1][1] == 'x' and squares[2][1] == 'x') or (squares[0][2] == 'x' and squares[1][2] == 'x' and squares[2][2] == 'x') or (squares[0][0] == 'x' and squares[1][1] == 'x' and squares[2][2] == 'x') or (squares[2][0] == 'x' and squares[1][1] == 'x' and squares[0][2] == 'x'):
+          print('x wins')
+          titletext.set('\nx Wins\n')
+        elif (squares[0][0] == 'o' and squares[0][1] == 'o' and squares[0][2] == 'o') or (squares[1][0] == 'o' and squares[1][1] == 'o' and squares[1][2] == 'o') or (squares[2][0] == 'o' and squares[2][1] == 'o' and squares[2][2] == 'o') or (squares[0][0] == 'o' and squares[1][0] == 'o' and squares[2][0] == 'o') or (squares[0][1] == 'o' and squares[1][1] == 'o' and squares[2][1] == 'o') or (squares[0][2] == 'o' and squares[1][2] == 'o' and squares[2][2] == 'o') or (squares[0][0] == 'o' and squares[1][1] == 'o' and squares[2][2] == 'o') or (squares[2][0] == 'o' and squares[1][1] == 'o' and squares[0][2] == 'o'):
+          print('o wins')
+          titletext.set('\no Wins\n')
         turns += 1
         if turns == 9:
             print('Draw')
             titletext.set('Draw')
 titletext = StringVar()
 titletext.set('Noughts\nand\nCrosses')
+
+
 ttk.Label(frame, textvariable=titletext).grid(column=1, row=0)
+
 
 i=0
 for x in range(3):
